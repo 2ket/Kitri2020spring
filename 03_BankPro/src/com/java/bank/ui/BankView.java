404d75d235eeaf485db1fa362aa097d6a7a20728
2ket <hello2ket@gmail.com>
2020-07-27 16:37:19 +0900

    출금, 잔액조회 혼자 해본것

--- 03_BankPro/src/com/java/bank/ui/BankView.java
+++ 03_BankPro/src/com/java/bank/ui/BankView.java
@@ -29,28 +29,32 @@ private BankDao bankDao;
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		printMenu();
 		
-		System.out.println("선택하세요.");
-		Scanner sc= new Scanner(System.in);
-		int choice=sc.nextInt();
+		boolean loop=true;
+		Scanner sc=null;
+		while(loop) {
 		
-		switch (choice) {
-		case 1: makeAccount();break;
-		case 2: deposit();break;
-		case 3: break;
-		case 4: break;
-		case 5: showData();break;
-		case 6: break;
+			printMenu();
+			sc=new Scanner(System.in);
+			System.out.println("선택하세요.");
+			int choice=sc.nextInt();
 			
-		
-		default:
-			System.out.println("잘못입력하셨습니다.");
+			switch (choice) {
+			case 1: makeAccount();break;
+			case 2: deposit();break;
+			case 3: withdraw();break;
+			case 4: inquiry();break;
+			case 5: showData();break;
+			case 6: loop=false;break;
+				
+			default:
+				System.out.println("잘못입력하셨습니다.");
+			}
 		}
-		
 		sc.close();
 	}
 	
+
 	public void printMenu() {
 		System.out.println("MENU------------------");
 		System.out.println("1. 계좌 개설");
@@ -63,18 +67,23 @@ private BankDao bankDao;
 	public void makeAccount() {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("계좌번호 : ");
-		
-		bankDto.setId(sc.next());
-		
-		System.out.println("이름 : ");
-		bankDto.setName(sc.next());
-		
-		System.out.println("입금액 : ");
-		bankDto.setBalance(sc.nextLong());
-		
-		int check=bankDao.makeAccount(bankDto);
-		LogAspect.logger.info(LogAspect.logMsg + check);
-		
+		String id=sc.next();
+		BankDto dto=bankDao.select(id);
+		if(dto != null) {
+			System.out.println("이미 존재하는 계좌번호입니다. 다른 번호를 입력해주세요.");
+		}else {
+			bankDto.setId(id);
+			System.out.println("이름 : ");
+			bankDto.setName(sc.next());
+			
+			System.out.println("입금액 : ");
+			bankDto.setBalance(sc.nextLong());
+			
+			int check=bankDao.makeAccount(bankDto);
+			LogAspect.logger.info(LogAspect.logMsg + check);
+			
+			System.out.println("계좌가 생성되었습니다.");
+		}
 		sc.close();
 	}
 	public void showData() {
@@ -105,6 +114,45 @@ private BankDao bankDao;
 			}else {
 				System.out.println("입금에 실패했습니다. 다시 시도해주세요.");
 			}
+		}else {
+			System.out.println("계좌번호가 존재하지 않습니다.");
+		}
+		
+		sc.close();
+	}
+
+	private void withdraw() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("계좌번호를 입력하세요.");
+		String id=sc.next();
+		bankDto=bankDao.select(id);
+		if(bankDto!=null) {
+			System.out.println("현재 잔액 : "+bankDto.getBalance());
+			System.out.println("출금하실 금액을 입력하세요.");
+			Long money=sc.nextLong();
+			if(money>bankDto.getBalance()) {
+				System.out.println("잔액이 부족합니다.");
+			}else {
+				bankDto.setBalance(bankDto.getBalance()-money);
+				int check=bankDao.update(bankDto);
+				if(check>0) {
+					System.out.println("출금완료.");
+				}else {
+					System.out.println("출금에 실패했습니다. 다시 시도해주세요.");
+				}
+			}
+		}else {
+			System.out.println("계좌번호가 존재하지 않습니다.");
+		}
+		sc.close();
+	}
+	public void inquiry() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("계좌번호를 입력하세요.");
+		String id=sc.next();
+		bankDto=bankDao.select(id);
+		if(bankDto!=null) {
+			System.out.println(bankDto.getName()+"님 현재 잔액 : "+bankDto.getBalance());
 		}else {
 			System.out.println("계좌번호가 존재하지 않습니다.");
 		}
